1be6814f11810b45bbfe9bd6448680747de0ecee
Siddhartha Singh <84330754+siddhartha-up80@users.noreply.github.com>
2022-09-26 19:34:03 +0530

    Track latest updates about Cryptocurrencies
    Cryptocurrency updates with real-time prices including price change per hour, price change per day and price change per hour.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import EnhancedTable from "./components/EnhancedTable";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import About from "./components/About";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [searchCoin, setSearchCoin] = useState("");
+  const [coinInfo, setCoinInfo] = useState([]);
+
+  useEffect(() => {
+    Axios.get(
+      "https://api.coinstats.app/public/v1/coins?skip=0&limit=100&currency=INR"
+    ).then((response) => {
+      setCoinInfo(response.data.coins);
+    });
+  }, []);
+
+  const filteredCoins = coinInfo.filter((coin) => {
+    return coin.name.toLowerCase().includes(searchCoin.toLowerCase());
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header setSearchCoin={setSearchCoin} />
+      <Routes>
+        <Route
+          path="/"
+          element={<EnhancedTable coinInfo={filteredCoins} />}
+        ></Route>
+
+        <Route path="about" element={<About />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
