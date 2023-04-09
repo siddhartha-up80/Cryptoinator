a11019088b575f23f4fd7c04e5c71db7c9868af4
Siddhartha Singh <84330754+siddhartha-up80@users.noreply.github.com>
2023-04-09 19:17:07 +0530

    Updated UI

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className="h-40  bg-blend-soft-light flex flex-col pt-10 justify-around">
       <div className="p-2">
-        <h2 className="font-bold mb-5 font-mono text-white text-6xl">
+        <h2 className="font-bold mb-5 font-mono text-white text-2xl md:text-6xl">
           CryptoInator
         </h2>
         <h2 className="text-white">Step in to the cryptocurrency world</h2>
